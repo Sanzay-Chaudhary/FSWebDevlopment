7acd79d585d64c40b47e3a78017b829f7d2f2728
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-11-28 07:41:10 +0545

    questionSet updated

--- quiz-app/app/questionSet/page.tsx
+++ quiz-app/app/questionSet/page.tsx
@@ -2,33 +2,41 @@ export const quiz = {
     questions: [
         {
             id: 1,
-            question: "What does API stand fro?",
-            answer: [
+            question: "What does API stand for?",
+            answers: [
                 "Application Programming Interface",
                 "Applied Programming Interface",
                 "Application Performance Index",
-                "Application Program Interface"],
+                "Application Program Interface"
+            ],
             correctAnswer: "Application Programming Interface",
         },
         {
             id: 3,
-            question: `Which programming language is often 
-			used for building web servers?`,
+            question: "Which programming language is often used for building web servers?",
             answers: ['Java', 'Python', 'JavaScript', 'C#'],
             correctAnswer: 'JavaScript',
         },
         {
             id: 4,
             question: 'What is the purpose of SQL?',
-            answers: ['Styling web pages', 'Querying databases',
-                'Creating animations', 'Developing mobile apps'],
+            answers: [
+                'Styling web pages',
+                'Querying databases',
+                'Creating animations',
+                'Developing mobile apps',
+            ],
             correctAnswer: 'Querying databases',
         },
         {
             id: 5,
             question: 'What does MVC stand for in web development?',
-            answers: ['Model View Controller', 'Model Visual Controller',
-                'Model View Component', 'Model Visual Component'],
+            answers: [
+                'Model View Controller',
+                'Model Visual Controller',
+                'Model View Component',
+                'Model Visual Component',
+            ],
             correctAnswer: 'Model View Controller',
         },
     ],
